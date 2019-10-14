4c51ec0451bfeb891c05b2b3d342bd6e60226ea5
Andrzej Kostanski <akos@novozymes.com>
2019-10-14 15:59:18 +0200

    demo for pytest-tap github issue #39 wither no ERROR inside tap file

---
+++ conftest.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+
+@pytest.fixture
+def some_fixture():
+    assert 2 == 3
---
+++ test_demo.py
@@ -0,0 +1,7 @@
+
+def test_basic_fail():
+    assert 2 == 3
+
+
+def test_basic_error(some_fixture):
+    assert 2 == 2
